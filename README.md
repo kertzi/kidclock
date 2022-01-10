f0e01c9f3b994427a88bf3cc85997d0871f74bab
Mikko Keränen <mikko.keranen@uros.com>
2022-01-10 14:56:56 +0200

    skeleton

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kidclock
 Clock to educate kids for daily routine who doesn't understand clock
+
+# setup
+
+`pip install wxpython`
---
+++ main.py
@@ -0,0 +1,35 @@
+import wx
+
+app_title = 'Kid Clock'
+
+class MainFrame(wx.Frame):
+  def __init__(self):
+    super().__init__(parent=None, title=app_title)
+    panel = wx.Panel(self)
+
+    box_sizer = wx.BoxSizer(wx.VERTICAL)
+
+    self.text_ctrl = wx.TextCtrl(panel)
+    press_btn = wx.Button(panel, label='Click me')
+    press_btn.Bind(wx.EVT_BUTTON, self.press_btn_on_press)
+
+    box_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
+    box_sizer.Add(press_btn, 0, wx.ALL | wx.CENTER, 5)
+
+    panel.SetSizer(box_sizer)
+
+    self.Show()
+
+  def press_btn_on_press(self, event):
+    value = self.text_ctrl.GetValue()
+    if not value:
+      print("No value")
+    else:
+      print(f'You typed: "{value}"')
+
+
+if __name__ == '__main__':
+  app = wx.App()
+  frame = MainFrame()
+  frame.Show()
+  app.MainLoop()
